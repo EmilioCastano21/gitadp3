a83c25f7d8e3a1f6c184b78f2f0f5513ecd9b84c
emilio castano <219035709@mycput.ac.za>
2021-03-25 19:42:07 +0200

    Included a comment. Extended the functionality of string length

--- src/za/ac/cput/gitadp3/System.java
+++ src/za/ac/cput/gitadp3/System.java
@@ -2,7 +2,12 @@ package za.ac.cput.gitadp3;
 
 public class System {
 
+    /**
+     *  This returns the length of the string
+     *
+     */
     public int lengthOfString(String str){
+        if(str == null || str.isEmpty()) return 0;
         return str.length();
     }
 }
